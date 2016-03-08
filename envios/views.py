a73d84a68e075c7342ee9a1b866d48809f44b659
Alberto <p02diada@gmail.com>
2016-03-08 19:01:24 +0100

    Calculo de distancias al pedir la lista de anuncios

--- envios/views.py
+++ envios/views.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+import math
 # Create your views here.
 
 
@@ -38,8 +39,58 @@ def crearAnuncio(request):
 @permission_classes((IsAuthenticated,))
 @api_view(['POST','PUT', 'GET'])
 def getAnuncios(request):
+	latitudeActual=request.POST.get('latitude','')
+	longitudeActual=request.POST.get('longitude','')
+
+	print "latitudeActual: "+ latitudeActual
+	print "longitudeActual: "+ longitudeActual
+
+	latitudActual=float(latitudeActual)
+	longitudActual=float(longitudeActual)
+
+
 
 	anuncios=Anuncio.objects.all()
+
+	for anuncio in anuncios:
+		latitud=anuncio.latitudPuntoFinal
+		longitud=anuncio.longitudPuntoInicial
+
+		print "latitud: " + latitud
+		print "longitud: " + longitud
+
+		latitud=float(latitud)
+		longitud=float(longitud)
+		metrosLongitudPuntoA=longitud*(10000000/90)
+		print metrosLongitudPuntoA
+		metrosLatitudPuntoA=latitud*(40000000/360)
+		print metrosLatitudPuntoA
+
+		metrosLongitudActual=longitudActual*(10000000/90)
+		print metrosLongitudActual
+		metrosLatitudActual=latitudActual*(40000000/360)
+		print metrosLatitudActual
+
+		diferenciaLongitudMetros=metrosLongitudPuntoA-metrosLongitudActual
+		print diferenciaLongitudMetros
+		diferenciaLatitudMetros=metrosLatitudPuntoA-metrosLatitudActual
+		print diferenciaLatitudMetros
+
+		diferenciaLongitudMetros=diferenciaLongitudMetros*diferenciaLongitudMetros
+		diferenciaLatitudMetros=diferenciaLatitudMetros*diferenciaLatitudMetros
+
+		sumaMetros=diferenciaLatitudMetros+diferenciaLongitudMetros
+
+		distacia=math.sqrt(sumaMetros)
+		print "distancia: "
+		print distacia
+
+
+
+
+
+
+
 	serializer=AnuncioSerializer(anuncios, many=True)
 	return Response(serializer.data)
 
